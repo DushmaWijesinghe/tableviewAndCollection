ca41b14fdf3602858be668c85c879fec86999c52
Digital-03 <>
2019-03-14 20:33:44 +0530

    inital commit

---
+++ tableViewAndCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewAndCollection
+//
+//  Created by Digital-03 on 3/14/19.
+//  Copyright © 2019 Digital-03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ tableViewAndCollectionTests/tableViewAndCollectionTests.swift
@@ -0,0 +1,34 @@
+//
+//  tableViewAndCollectionTests.swift
+//  tableViewAndCollectionTests
+//
+//  Created by Digital-03 on 3/14/19.
+//  Copyright © 2019 Digital-03. All rights reserved.
+//
+
+import XCTest
+@testable import tableViewAndCollection
+
+class tableViewAndCollectionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
